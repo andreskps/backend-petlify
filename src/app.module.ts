b7744ea52a6a965913c2c444001fb6a11e318633
andreskps <pipesierra146@gmail.com>
2024-03-26 07:49:33 -0500

    add config db and typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,30 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [
+
+    ConfigModule.forRoot({isGlobal: true}),
+
+
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      database: process.env.DB_NAME,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      autoLoadEntities: true,
+      synchronize: true, // Sirve para detectar cambios en la db, se coloca false en produccion
+      ssl:
+        process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false,
+    }),
+
+    ProductsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,1 +1,80 @@
-export class Product {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Product {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column('text')
+  title: string;
+
+  @Column('float', {
+    nullable: false,
+  })
+  price: number;
+
+  @Column('float', {
+    nullable: true,
+  })
+  priceCompare: number;
+
+
+  @Column('text')
+  description: string;
+
+
+
+  // @ManyToOne(() => User, user => user.products)
+  // user: User;
+
+  // @ManyToOne(() => Category, category => category.products)
+  // category: Category;
+
+  // @OneToMany(() => OrderDetail, orderDetail => orderDetail.product)
+  // orderDetails: OrderDetail[];
+
+  // @OneToMany(() => Cart, cart => cart.product)
+  // carts: Cart[];
+
+  // @OneToMany(() => Review, review => review.product)
+  // reviews: Review[];
+
+  // @OneToMany(() => WishList, wishList => wishList.product)
+  // wishLists: WishList[];
+
+  // @OneToMany(() => ProductTag, productTag => productTag.product)
+  // productTags: ProductTag[];
+
+  // @OneToMany(() => ProductImage, productImage => productImage.product)
+  // productImages: ProductImage[];
+
+  // @OneToMany(() => ProductAttribute, productAttribute => productAttribute.product)
+  // productAttributes: ProductAttribute[];
+
+  // @OneToMany(() => ProductDiscount, productDiscount => productDiscount.product)
+  // productDiscounts: ProductDiscount[];
+
+  // @OneToMany(() => ProductSpecification, productSpecification => productSpecification.product)
+  // productSpecifications: ProductSpecification[];
+
+  // @OneToMany(() => ProductVariant, productVariant => productVariant.product)
+  // productVariants: ProductVariant[];
+
+  // @OneToMany(() => ProductOption, productOption => productOption.product)
+  // productOptions: ProductOption[];
+
+  // @OneToMany(() => ProductReview, productReview => productReview.product)
+  // productReviews: ProductReview[];
+
+  // @OneToMany(() => ProductCategory, productCategory => productCategory.product)
+  // productCategories: ProductCategory[];
+
+  // @OneToMany(() => ProductAttribute, productAttribute => productAttribute.product)
+  // productAttributes: ProductAttribute[];
+
+  // @OneToMany(() => ProductTag, productTag => productTag.product)
+  // productTags: ProductTag[];
+
+  // @OneToMany(() => ProductImage, productImage => productImage.product)
+  // productImages: ProductImage[];
+}
