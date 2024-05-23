86770158787a912fb47b08e16ddf4ad361a5033c
andreskps <pipesierra146@gmail.com>
2024-05-23 11:23:08 -0500

    chore: Remove unused files and update gitignore

--- src/email/email.controller.ts
+++ src/email/email.controller.ts
@@ -2,7 +2,6 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { EmailService } from './email.service';
 import { CreateEmailDto } from './dto/create-email.dto';
 import { UpdateEmailDto } from './dto/update-email.dto';
-import Email from 'emails/example';
 
 @Controller('email')
 export class EmailController {
