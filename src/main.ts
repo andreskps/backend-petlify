9be4a5d99afdbf5f94a02551b6d203f9b27cd470
andreskps <pipesierra146@gmail.com>
2024-06-07 08:38:49 -0500

    feat: Update CORS configuration to allow specific origins

--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,45 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api'); // prefijo para todas las rutas
   // app.useGlobalFilters(new HttpExceptionFilter());
-  
-  app.enableCors(); // habilita cors
+
+  const allowedOrigins = [process.env.URL_ECOMMERCE, process.env.URL_ADMIN];
+
+  const corsOptions: CorsOptions = {
+    origin: (origin, callback) => {
+      console.log('Origin:', origin);
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+      
+        callback(null, false);
+      }
+    },
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  };
+
+  app.enableCors(corsOptions);
+
+  // Middleware para manejar errores CORS
+  app.use((req, res, next) => {
+    if (
+      !req.headers.origin ||
+      allowedOrigins.indexOf(req.headers.origin) !== -1
+    ) {
+      next();
+    } else {
+      res.status(403).json({ message: 'Not allowed by CORS' });
+    }
+  });
 
   app.use(cookieParser());
 
-  
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -23,6 +51,14 @@ async function bootstrap() {
       },
     }),
   );
+
+  // app.enableCors({
+  //   origin:[
+  //     'http://localhost:3001'
+  //   ],
+  //   methods:['GET','POST','PUT','DELETE','PATCH','OPTIONS'],
+  //   credentials:true
+  // }); // habilita cors
   await app.listen(3000);
 }
 bootstrap();
